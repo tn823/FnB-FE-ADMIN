1c9dc23ccc37c609a25ce39bd445094de6de46a4
tn823 <trnghia823@gmail.com>
2024-10-05 14:29:57 +0700

    update: areacard

--- src/components/dashboard/areaCards/AreaCard.jsx
+++ src/components/dashboard/areaCards/AreaCard.jsx
@@ -1,49 +1,44 @@
 import PropTypes from "prop-types";
-import { PieChart, Pie, Cell, Tooltip } from "recharts";
+import {
+  DollarOutlined,
+  CheckCircleOutlined,
+  InfoCircleFilled,
+} from "@ant-design/icons";
 
-const AreaCard = ({ colors, percentFillValue, cardInfo }) => {
-  const filledValue = (percentFillValue / 100) * 360;
-  const remainedValue = 360 - filledValue;
-
-  const data = [
-    { name: "Remained", value: remainedValue },
-    { name: "Achieved Sales", value: filledValue },
-  ];
-
-  const renderTooltipContent = (value) => {
-    return `${(value / 360) * 100} %`;
-  };
+const AreaCard = ({ colors, cardInfo }) => {
+  // Lựa chọn icon dựa trên tiêu đề của từng card
+  let IconComponent;
+  switch (cardInfo.title) {
+    case "DOANH THU": // Khớp với tiêu đề truyền vào từ AreaCards
+      IconComponent = DollarOutlined;
+      break;
+    case "HOÀN THÀNH": // Khớp với tiêu đề truyền vào từ AreaCards
+      IconComponent = CheckCircleOutlined;
+      break;
+    case "ĐANG ĐỢI": // Khớp với tiêu đề truyền vào từ AreaCards
+      IconComponent = InfoCircleFilled;
+      break;
+    default:
+      IconComponent = null;
+  }
 
   return (
     <div className="area-card">
       <div className="area-card-info">
+        {/* Hiển thị icon trước tiêu đề */}
+        <div className="info-icon">
+          {IconComponent && (
+            <IconComponent style={{ fontSize: 24, color: colors[1] }} />
+          )}
+        </div>
         <h5 className="info-title">{cardInfo.title}</h5>
-        <div className="info-value">{cardInfo.value}</div>
+        <div
+          className="info-value"
+          style={{ color: colors[1] }} // Màu chữ dựa trên giá trị truyền vào
+        >
+          {cardInfo.value}
+        </div>
         <p className="info-text">{cardInfo.text}</p>
-      </div>
-      <div className="area-card-chart">
-        <PieChart width={100} height={100}>
-          <Pie
-            data={data}
-            cx={50}
-            cy={45}
-            innerRadius={20}
-            fill="#e4e8ef"
-            paddingAngle={0}
-            dataKey="value"
-            startAngle={-270}
-            endAngle={150}
-            stroke="none"
-          >
-            {data.map((entry, index) => (
-              <Cell
-                key={`cell-${index}`}
-                fill={colors[index % colors.length]}
-              />
-            ))}
-          </Pie>
-          <Tooltip formatter={renderTooltipContent} />
-        </PieChart>
       </div>
     </div>
   );
@@ -53,6 +48,5 @@ export default AreaCard;
 
 AreaCard.propTypes = {
   colors: PropTypes.array.isRequired,
-  percentFillValue: PropTypes.number.isRequired,
   cardInfo: PropTypes.object.isRequired,
 };
